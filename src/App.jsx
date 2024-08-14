786aec2b2be35efb06deb61c7edc999aa9113071
Mahesh KN <maheshdsu86@gmail.com>
2024-08-15 03:13:09 +0530

    test commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <>
-      <h1 className='text-5xl font-bold'>Hello wp-ginbjgnffd</h1>
+      <h1 className='text-5xl font-bold'>Hello WORLD bruuuuuuuuuuuh</h1>
     </>
   )
 }
